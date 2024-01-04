378617b9923101dc4804beda426d426e66f3f348
tokkaya79 <tokkaya79@gmail.com>
2024-01-04 10:58:05 +0100

    created header, home, login, movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieComponent } from './movie/movie.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    MovieComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  username = "";
+  password = "";
+  errorMsg = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  login(){
+    if (this.username.trim().length === 0){
+      this.errorMsg = "Username is required"
+    } else if (this.password.trim().length === 0){
+      this.errorMsg = "Password is required"
+    } else {
+      this.errorMsg = "";
+    }
+  }
+}
