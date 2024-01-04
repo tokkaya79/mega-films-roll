ab39c9e576f7a9f457693254ef7cf0d947ca0a68
tokkaya79 <tokkaya79@gmail.com>
2024-01-04 11:36:09 +0100

    header

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
   login(uname: string, pword: string) {
     if (uname === 'suren' && pword ==='1234') {
       return 200
@@ -13,4 +14,9 @@ export class AuthService {
       return 403
     }
   }
+  logout(){
+    this.router.navigate(['login'])
+
+  }
+
 }
