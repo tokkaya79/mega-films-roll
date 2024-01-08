62e744514c163c919513467d7e0a35b0b0e27255
tokkaya79 <tokkaya79@gmail.com>
2024-01-08 21:08:38 +0100

    added name and pasword

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 export class AuthService {
   constructor(private router: Router) {}
   login(uname: string, pword: string) {
-    if (uname === 'suren' && pword === '1234') {
+    if (uname === 'vala' && pword === '1313') {
       return 200;
     } else {
       return 403;
